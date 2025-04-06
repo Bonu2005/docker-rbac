ec948599fd19ea3f32b76eb3fda4c5301381de03
Bonu2005 <booonu@icloud.com>
2025-04-06 16:57:26 +0500

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { UploadsModule } from './uploads/uploads.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
+import { ColorModule } from './color/color.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UploadsModule, ServeStaticModule.forRoot({rootPath:join(__dirname,"..","uploads"),serveRoot:'/file'})],
+  imports: [UploadsModule, ServeStaticModule.forRoot({rootPath:join(__dirname,"..","uploads"),serveRoot:'/file'}), AuthModule, PrismaModule, CategoryModule, ProductModule, ColorModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
